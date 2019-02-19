9403097a6fe114a92b06533b04ea626ba566db4f
Thiago Rosa Ataide <thiagorozaa@macbook-air-de-thiago.local>
2019-02-19 01:53:39 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Thiago Rosa Ataide on 19/02/19.
+//  Copyright © 2019 Thiago Rosa Ataide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
